24bba7a0741050d10eaf92f348f78197c08e10c7
wv_todoz <kauan@facileme.com.br>
2019-01-28 16:18:29 -0200

    menu e banner finalizado

--- wv_2019/index.php
+++ wv_2019/index.php
@@ -1,1 +1,91 @@
-<? echo "teste"; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=0">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>Site Wv 2019</title>    
+
+    <!-- Bootstrap core CSS -->    
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+  
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <div class="menu">
+          <nav>
+            <ul>
+              <li class="active"><a href="#Quem Somos">Quem Somos</a></li>
+              <li><a href="#">Home</a></li>      
+              <li><a href="#Portifolio">Portifólio</a></li>
+              <li><a href="#Clientes">Clientes</a></li>
+              <li><a href="#Blog">Blog</a></li>
+              <li><a href="#Equipe">Equipe</a></li>
+              <li><a href="#Contato">Contato</a></li>
+            </ul>           
+          </nav>                  
+        </div>        
+        <div class="phones">
+          <ul>
+            <li><a><span>(11)</span> 5531-1779</a></li>
+            <li><a><span>(11)</span> 5531-1787</a></li>
+          </ul>
+        </div>  
+      </div>          
+    </header>
+    <section id="banner">
+      <div class="container column">
+        <div class="banner_logo">
+          <img src="assets/images/logo.fw.png">          
+        </div>
+        <div class="banner_textos">
+          <hgroup>
+            <h1>Seja Todoz<br> com a gente!</h1>         
+            <h3>Somos especialistas no desenvolvimento<br> de aplicativos para Facebook e Mobile.</h3>          
+          </hgroup>
+        </div> 
+        <div class="banner_t3">
+          <div class="t3">
+            <p>Você precisa que a sua empresa seja vista na Web a qualquer horas e em qualquer lugar?<p>
+
+            <p>Você veio ao lugar certo! Convertemos ideias em aplicativos Web para:
+            Facebook, twitter, IOS, android, que tenham o seu conceito, e que se identifique com seu público.</p>
+
+            <p>Fique tranquilo, a sua empresa está nas mãos certas de quem fala a linguagem dos aplicativos.</p>
+          </div>          
+        </div>     
+    </section>
+          
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>    
+  </body>
+</html>
