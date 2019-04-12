7ba98d1aef00d5f289f3906e60dd7095566d7eb7
wv_todoz <kauan@facileme.com.br>
2019-04-12 17:54:55 -0300

    manipulando arrays

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -26,11 +26,11 @@ linhas
 
 }*/
 
-function somar(){
+/*function somar(){
 	var campo1 = parseInt(document.getElementById("campo1").value);
 	var campo2 = parseInt(document.getElementById("campo2").value);
 
 	var soma = campo1 + campo2;
 
 	alert(soma);
-}
+}*/
