019f41b8939993398d91c4dbfc4398b8f21445e3
wv_todoz <kauan@facileme.com.br>
2019-04-15 16:49:32 -0300

    Aulas JS Basico concluida

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -34,3 +34,82 @@ linhas
 
 	alert(soma);
 }*/
+
+/*function testeVerificador(){
+	var x = prompt("Diga um número:");
+
+	if (x >= 10){
+		alert("É maior ou igual 10!!!");
+	}else{
+		alert("Não é maior que 10!");
+	}
+}*/
+
+/*
+------------------------------------------
+	While
+	var x = 0;
+
+	document.write("Iniciando o Loop...<br/><br/>");
+
+	while(x < 10){
+		document.write("Numero: "+x+"<br/>");
+		x++;
+	}
+
+	document.write("<br/>Finalizando o Loop...<br/>");
+------------------------------------------
+*/
+
+/*
+------------------------------------------
+	For
+	obs. No for eu defino a variavel, determino a condição, incremento, depois jogo na tela
+
+	for(x = 0; x < 10; x++){
+		document.write("Numero "+x+"<br/>");
+	}
+------------------------------------------
+*/
+
+/*
+------------------------------------------
+	Switch
+	x = prompt("Qual é o número?");
+
+		switch(x){
+			case "0":
+				alert("X é o 0");
+				break;
+			case "1":
+				alert("X é o 1");
+				break;
+			case "2":
+				alert("X é o 2");
+				break;
+			default:
+				alert("Ninguém foi satisfeito!");
+				break;
+		}
+------------------------------------------
+*/
+
+function verificar(){
+	var n1 = document.getElementById("n1").innerHTML;
+	var n2 = document.getElementById("n2").value;
+
+	if(n1 == n2){
+		alert("VOCÊ ACERTOU O NÚMERO!");
+	}else{
+		alert("VOCÊ ERROU!");
+	}
+
+	resetar();
+}
+
+function resetar(){
+	document.getElementById("n2").value = "";
+
+	var r = Math.floor(Math.random() * 100);
+	document.getElementById("n1").innerHTML = r;	
+}
