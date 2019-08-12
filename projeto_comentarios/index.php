b2a7bfd2596a107498f04e37f5f03454ba913446
Tiago <tiago@wvtodoz.com.br>
2019-08-12 16:01:54 -0300

    projeto comentários finalizado

--- projeto_comentarios/index.php
+++ projeto_comentarios/index.php
@@ -1,12 +1,17 @@
+<head>
+	<meta charset="utf-8">
+</head>
+
 <?php
 try{
-	$pdo = new PDO("mysql:dbname=projeto_comentarios;host=localhost", "wvtodoz", "senha");
+	$pdo = new PDO("mysql:dbname=projeto_comentarios;host=localhost;charset=utf8", "wvtodoz", "senha");
 } catch(PDOException $e) {
 	echo "Erro: ".$e->getMessage();
 	exit;
 }
-var_dump($_POST);
-/*exit;*/
+/*var_dump($_POST);
+exit;*/
+
 if(isset($_POST['nome']) && empty($_POST['nome']) == false){
 	$nome = $_POST['nome'];
 	$mensagem = $_POST['mensagem'];
@@ -31,7 +36,28 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false){
 <br/><br/>
 
 <?php
-	$c = 0;
+
+/*
+1. calcular a quantidade total de páginas.
+2. definir o $c.
+3. fazer a query com LIMIT
+*/
+	$qt_por_pagina = 6;
+
+	$total = 0;
+	$sql = "SELECT COUNT(*) as c FROM mensagens";
+	$sql = $pdo->query($sql);
+	$sql = $sql->fetch();
+	$total = $sql['c'];
+	$paginas = $total / $qt_por_pagina;		
+
+	$pg = 1;
+	if(isset($_GET['c']) && !empty($_GET['c'])){
+		$pg = addslashes($_GET['c']);
+	}
+	$c = ($pg - 1) * $qt_por_pagina;
+
+
 	$sql = "SELECT *,date_format(data_msg,'%d-%m-%Y') as data_msg_formatada FROM mensagens ORDER BY data_msg DESC LIMIT $c, 5";
 	$sql = $pdo->query($sql);
 	if($sql->rowCount() > 0){ 
@@ -46,7 +72,10 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false){
 	} else {
 		echo "Não há mensagens.";
 	}
+
+	echo "<hr/>";
+	for($q=0;$q<$paginas;$q++){
+		echo '<a href="./?c='.($q+1).'">['.($q+1).']</a>';
+	} 
+	
 ?>
-
-
-
