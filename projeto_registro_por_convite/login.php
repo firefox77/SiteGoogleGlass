8dae463fe503670fc46670c9c0340f33b498e737
Tiago <tiago@wvtodoz.com.br>
2019-08-12 17:59:49 -0300

    projeto cadastrar com permissão

---
+++ projeto_registro_por_convite/login.php
@@ -0,0 +1,9 @@
+<form method="POST">
+	E-mail:<br/>
+	<input type="text" name="email"><br/> <br/>	
+	Senha:<br>
+	<input type="password" name="senha"><br/>
+	<br/>
+	<input type="submit" value="Entrar">	
+	<a href="./">Cadastrar</a>
+</form>
