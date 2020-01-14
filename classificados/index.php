7be821eaa2c1828a4d18cdf7b778484992e590fa
Tiago <tiago@wvtodoz.com.br>
2020-01-14 17:58:17 -0300

    iniciando index do projeto classificados

---
+++ classificados/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Classificados</title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script> <!--jquery sempres antes do bootstrap-->
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
+		<script type="text/javascript" src="assets/js/script.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse"><!--usando bootstrap. O navbar-inverse deixa o fundo escuro-->
+			<div class="container-fluid"><!--container-fluid pega de ponta a ponta-->
+				<div class="navbar-header">
+					<a href="./" class="navbar-brand">Classificados</a>					
+				</div>
+				<ul class="nav navbar-nav navbar-right"><!--navbar-nav deixa os elementos um do lado do outro-->
+					<li><a href="">Cadastre-se</a></li>
+					<li><a href="">Login</a></li>
+				</ul>		
+			</div>			
+		</nav>
+		<div class="container-fluid">
+			<div class="jumbotron">
+				<h2>NóS temos hoje 999 anúncios</h2>				
+				<p>E mais de 999 usuários cadastrados.</p>
+			</div>			
+		</div>
+	</body>
+</html>
